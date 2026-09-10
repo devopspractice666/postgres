5243432a7339b719d7ce328cdc49948c197084ca
devopspractice666 <mskazachenok_3_zubovich@mail.ru>
2026-09-10 17:38:08 +0300

    added graceful shutdown to application

--- project/application/main.go
+++ project/application/main.go
@@ -2,17 +2,22 @@ package main
 
 import (
 	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	. "myapp/connection"
 	. "myapp/db_operations"
 	. "myapp/postgres"
-	"net/http"
 
 	"github.com/gorilla/mux"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
-
 	conn := GetConn()
 	router := mux.NewRouter()
 	database := &Postgres{Conn: conn}
@@ -34,5 +39,34 @@ func main() {
 
 	router.Path("/metrics").Methods("GET").Handler(promhttp.Handler())
 
-	http.ListenAndServe(":9055", router)
+	srv := &http.Server{
+		Addr:    ":9055",
+		Handler: router,
+	}
+
+	go func() {
+		log.Println("Запуск сервера на порту :9055")
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Ошибка сервера: %v", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	<-quit
+	log.Println("Остановка сервера...")
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(shutdownCtx); err != nil {
+		log.Fatalf("Сервер принудительно остановлен: %v", err)
+	}
+
+	if err := conn.Close(shutdownCtx); err != nil {
+		log.Printf("Ошибка закрытия соединения с БД: %v", err)
+	}
+
+	log.Println("Сервер завершил работу корректно")
 }
